1a35a8c072a3d34cd6216117129f65d8fa283ffa
zelsx <zhongink@gmail.com>
2017-07-25 23:50:39 +0800

    Swap_Nodes_in_Pairs

---
+++ Algorithms/leetcode/Swap_Nodes_in_Pairs.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head)
+        return null;
+    var arr = [];
+    while (head) {
+        var next = head.next;
+        head.next = null;
+        arr.push(head);
+        head = next;
+    }
+    for (var i= 0; i< arr.length; i=i+2){
+        var a = arr[i],
+            b = arr[i+1];
+        if(!b) continue;
+        arr[i] = b;
+        arr[i + 1] = a;
+    }
+    for (var i = 0; i < arr.length;i++){
+        arr[i].next = arr[i+1];
+    }
+    return arr[0];
+};
