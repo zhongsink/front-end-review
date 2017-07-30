fb854c8a2a3ecb8894b41677ea0005a59ef741d7
zelsx <zhongink@gmail.com>
2017-07-31 00:10:13 +0800

    encode_and_Decode

--- Algorithms/leetcode/Encode_and_Decode_TinyURL.js
+++ Algorithms/leetcode/Encode_and_Decode_TinyURL.js
@@ -1,27 +1,15 @@
-let [p, index] = [new Map(), 0];
-
-var base62 = (n) => {
-  let str = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ';
-  let len = str.length;
-  let ret = '';
-
-  do {
-    ret += str[n % len];
-    n = ~~(n / len);
-  } while (n);
-
-  return ret;
-};
-
 /**
  * Encodes a URL to a shortened URL.
  *
  * @param {string} longUrl
  * @return {string}
+ * encode => 长地址 映射到短地址
+ * decode => 从短地址获取长地址
  */
+var map = new Map();
 var encode = function(longUrl) {
-  let shortUrl = base62(index++);
-  p.set(shortUrl, longUrl);
+  let shortUrl = +(new Date());
+  map.set(shortUrl, longUrl);
   return shortUrl;
 };
 
@@ -32,5 +20,5 @@ var encode = function(longUrl) {
  * @return {string}
  */
 var decode = function(shortUrl) {
-  return p.get(shortUrl);
+  return map.get(shortUrl);
 };
